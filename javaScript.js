17d9e5e829145cb204fc340c3f3c840d53295c4f
Bastian Velasquez <basti14velasquez@gmail.com>
2025-04-17 09:03:02 -0400

    Change UI and Add function to add book to the array with the form. The books are viewed in the page

--- javaScript.js
+++ javaScript.js
@@ -27,23 +27,83 @@ cancel.addEventListener("click", () => {
 
 const form = document.querySelector("form");
 
-function renderLibrary(){
+function renderLibrary(book){
     const main = document.querySelector("main");
+
     const card = document.createElement("div");
     card.classList.add("book-card");
+
+    const titleBook = document.createElement("p");
+    titleBook.classList.add("title-card");
+    titleBook.textContent = book.title;
+
+    const authorBook = document.createElement("p");
+    authorBook.classList.add("author-card");
+    authorBook.textContent = "Author: " + book.author;
+
+    const pagesBook = document.createElement("p");
+    pagesBook.classList.add("pages-card");
+    pagesBook.textContent = "Number of pages: " + book.pages;
+
+    const readBook = document.createElement("p");
+    readBook.classList.add("read-card");
+    readBook.textContent = "Read: " + (book.read ? "Yes" : "No");
+
+    const buttonsBook = document.createElement("div");
+    buttonsBook.classList.add("buttons-book");
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-button");
+    deleteButton.textContent = "DELETE";
+
+    const readButton = document.createElement("button");
+    readButton.classList.add("read-button");
+    readButton.textContent = "READ";
+
+    buttonsBook.appendChild(deleteButton);
+    buttonsBook.appendChild(readButton);
+
     main.appendChild(card);
+    card.appendChild(titleBook);
+    card.appendChild(authorBook);
+    card.appendChild(pagesBook);
+    card.appendChild(readBook);
+    card.appendChild(buttonsBook);
+
+    card.dataset.id = book.id;
+
+    deleteButton.addEventListener("click", () =>{
+        main.removeChild(card);
+
+        const index = myLibrary.findIndex(b => b.id === book.id);
+        if (index !== -1){
+            myLibrary.splice(index,1);
+        }
+        console.log(myLibrary)
+    })
+
+    readButton.addEventListener("click", () => {
+        book.read = !book.read;
+        readBook.textContent = "Read: " + (book.read ? "Yes" : "No");
+        });
+
+    console.log(myLibrary)
 }
 
 form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const pag = document.getElementById("pag").value;
     const read = document.getElementById("read").checked;
-    addBookToLibrary(title, author, pag, read);
-    console.log(myLibrary);
-    dialog.close();
-    event.preventDefault();
-});
 
-console.log(myLibrary)
+    addBookToLibrary(title, author, pag, read);
+    const newBook = myLibrary[myLibrary.length-1]
+    renderLibrary(newBook);
+
+    console.log(myLibrary);
+
+    dialog.close();
+});
 
